d58b7d304f0b2d13bc77a6f7b2b488ccc29d1ff6
Danrejk <SDanrejk@gmail.com>
2024-01-28 22:52:38 +0100

    Add project files.

---
+++ Plugin.cs
@@ -0,0 +1,14 @@
+﻿using BepInEx;
+
+namespace DiceScrap
+{
+    [BepInPlugin(PluginInfo.PluginGuid, PluginInfo.PluginName, PluginInfo.PluginVersion)]
+    public class Plugin : BaseUnityPlugin
+    {
+        private void Awake()
+        {
+            // Plugin startup logic
+            Logger.LogInfo($"Plugin {PluginInfo.PluginGuid} is loaded!");
+        }
+    }
+}
