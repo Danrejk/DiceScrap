36b8c9769be8e82b8730bb5297baee8d621f4288
Danrejk <SDanrejk@gmail.com>
2024-01-29 20:38:46 +0100

    Die unity data

--- Plugin.cs
+++ Plugin.cs
@@ -1,4 +1,12 @@
-﻿using BepInEx;
+﻿using System;
+using System.IO;
+using System.Reflection;
+using BepInEx;
+using BepInEx.Configuration;
+using BepInEx.Logging;
+using LethalLib.Modules;
+using UnityEngine;
+using NetworkPrefabs = LethalLib.Modules.NetworkPrefabs;
 
 namespace DiceScrap
 {
@@ -6,10 +14,34 @@ namespace DiceScrap
     [BepInDependency(LethalLib.Plugin.ModGUID)]
     public class Plugin : BaseUnityPlugin
     {
+        public static AssetBundle MyCustomAssets;
+        public Item piosikDie;
+
         private void Awake()
         {
+            InitPiosikDie();
             // Plugin startup logic
             Logger.LogInfo($"Plugin {PluginInfo.PLUGIN_GUID} is loaded!");
+        }
+
+        private void InitPiosikDie()
+        {
+            var assetDir = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location) ?? throw new InvalidOperationException(), "landmine_placer");
+            var assetBundle = AssetBundle.LoadFromFile(assetDir);
+
+            MyCustomAssets = AssetBundle.LoadFromFile(Path.Combine(assetDir, "piosikdiedata"));
+            if (MyCustomAssets == null)
+            {
+                Logger.LogError("Failed to load custom assets."); // ManualLogSource for your plugin
+                return;
+            }
+
+            var rarity = 500; //30
+            piosikDie = assetBundle.LoadAsset<Item>("Assets/piosikdiedata.asset");
+            LethalLib.Modules.Utilities.FixMixerGroups(piosikDie.spawnPrefab);
+            LethalLib.Modules.NetworkPrefabs.RegisterNetworkPrefab(piosikDie.spawnPrefab);
+            LethalLib.Modules.Items.RegisterScrap(piosikDie, rarity, LethalLib.Modules.Levels.LevelTypes.All);
+
         }
     }
 }
