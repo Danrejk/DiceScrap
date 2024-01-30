8004df40372227a85824e579aa556890ea020ccb
Danrejk <SDanrejk@gmail.com>
2024-01-30 22:21:39 +0100

    Fixed rarity, added Easter Egge loading message. Changed name from "Dice Scrap" to "Piosik Dice"

--- Plugin.cs
+++ Plugin.cs
@@ -16,13 +16,15 @@ namespace DiceScrap
     public class Plugin : BaseUnityPlugin
     {
         public static AssetBundle MyCustomAssets;
-        public Item piosikDie;
+        public Item PiosikDice;
 
         private void Awake()
         {
             InitPiosikDie();
             // Plugin startup logic
-            Logger.LogInfo($"Plugin {PluginInfo.PLUGIN_GUID} is loaded!!!!!!!!!!!!!!!!");
+            Logger.LogInfo("Dzień dobry, proszę o chwilę cierpliwości.");
+            Logger.LogInfo($"Plugin {PluginInfo.PLUGIN_NAME} is loaded!");
+            Logger.LogWarning("Proszę Państwa, mam w domu awarię wodną, dlatego sposób przekazania wyniku prac nad portfolio podam później.");
         }
 
         private void InitPiosikDie()
@@ -30,14 +32,14 @@ namespace DiceScrap
             var assetDir = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location) ?? throw new InvalidOperationException(), "piosikdiedata");
             var assetBundle = AssetBundle.LoadFromFile(assetDir);
 
-            piosikDie = assetBundle.LoadAsset<Item>("Assets/piosikdiedata.asset");
-            if (piosikDie == null)
+            PiosikDice = assetBundle.LoadAsset<Item>("Assets/piosikdiedata.asset");
+            if (PiosikDice == null)
             {
                 Logger.LogError("Could not load piosikdiedata item");
                 return;
             }
 
-            var piosikDiePrefab = piosikDie.spawnPrefab;
+            var piosikDiePrefab = PiosikDice.spawnPrefab;
             if (piosikDiePrefab == null)
             {
                 Logger.LogError("Could not load piosikdiedata prefab");
@@ -52,10 +54,10 @@ namespace DiceScrap
             }
 
 
-            var rarity = 5000; //30
-            LethalLib.Modules.Utilities.FixMixerGroups(piosikDie.spawnPrefab);
-            LethalLib.Modules.NetworkPrefabs.RegisterNetworkPrefab(piosikDie.spawnPrefab);
-            LethalLib.Modules.Items.RegisterScrap(piosikDie, rarity, LethalLib.Modules.Levels.LevelTypes.All);
+            var rarity = 30; //30
+            LethalLib.Modules.Utilities.FixMixerGroups(PiosikDice.spawnPrefab);
+            LethalLib.Modules.NetworkPrefabs.RegisterNetworkPrefab(PiosikDice.spawnPrefab);
+            LethalLib.Modules.Items.RegisterScrap(PiosikDice, rarity, LethalLib.Modules.Levels.LevelTypes.All);
 
         }
     }
